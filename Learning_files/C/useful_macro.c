5db3b4cc917076bee901fae77903e238f2998405
QihaoYe <11610812@mail.sustc.edu.cn>
2017-10-27 00:53:58 +0800

    update

--- Learning_files/C/useful_macro.c
+++ Learning_files/C/useful_macro.c
@@ -2,6 +2,8 @@
 
 
 #define swap(a,b) a=a^b;b=a^b;a=a^b
+#define string(a) #a
+#define join(a,b) a##b
 
 
 // ---[test zone]---
@@ -9,7 +11,10 @@ int main(int argc, char const *argv[])
 {
 	int a = 2;
 	int b = 4;
+	int join(a, b) = 8;
 	swap(a, b);
 	printf("%d %d\n", a, b);
+	printf("%s\n", string(swap(a, b)));
+	printf("%d\n", join(a, b));
 	return 0;
 }
